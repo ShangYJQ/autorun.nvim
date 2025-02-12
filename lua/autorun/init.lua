73ebd4e64f0d4ce28cf07e30ee0d1afab822d61b
ShangYJQ <421207553@qq.com>
2025-02-12 19:30:50 +0000

    fix

--- lua/autorun/init.lua
+++ lua/autorun/init.lua
@@ -1,5 +1,5 @@
-local coref = require("autorun.core")
-local defult_conf = require("autorun.conf")
+local coref = require("autorun.core.lua")
+local defult_conf = require("autorun.conf.lua")
 local conf = {}
 local M = {}
 
