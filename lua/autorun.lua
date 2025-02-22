f5af4079117568f702ed9f0c8ed0aa961a8565c5
ShangYJQ <421207553@qq.com>
2025-02-22 16:14:54 +0000

    bak

--- lua/autorun.lua
+++ lua/autorun.lua
@@ -1,6 +1,5 @@
 local coref = require("autorun.utils")
 local defult_conf = require("autorun.config")
-local dap_core = require("autorun.dapcore")
 local conf = {}
 local M = {}
 
@@ -38,37 +37,12 @@ function M.run_code()
   vim.api.nvim_command(":TermExec direction=float cmd='" .. cmd .. "'")
 end
 
-function M.dap_code()
-  local file_path = vim.fn.expand("%:p")
-  local file_type = vim.fn.expand("%:e")
-  local file_name = coref.get_file_name(file_path)
-  local out_file_name = tostring(os.time())
-  local out_file_path = coref.get_file_dir(file_path) .. "/debug/" .. out_file_name
-
-  if not file_type == "cpp" then
-    error("The file type is not cpp!")
-  end
-  local debug_dir = coref.get_file_dir(file_path) .. "/debug"
-  os.execute("rm -rf " .. debug_dir .. " && mkdir " .. debug_dir)
-
-  local cmd = conf.cpp_dap_c .. " -g " .. file_name .. " -o debug/" .. out_file_name
-  cmd = "cd " .. coref.get_file_dir(file_path) .. " && " .. cmd
-
-  vim.api.nvim_command(":!" .. cmd)
-
-  dap_core.cpp_dap(out_file_path)
-  vim.api.nvim_command(":lua require'dap'.continue()")
-end
-
 function M.setup(opts)
   conf = vim.tbl_deep_extend("force", defult_conf, opts or {})
   vim.api.nvim_create_user_command("Autorun", M.run_code, {
     desc = "Use autorun plugin to run your code!",
   })
-  vim.api.nvim_create_user_command("Autodap", M.dap_code, {
-    desc = "Use autorun plugin to dap your code!"
-  })
-  -- vim.keymap.set("n", "<A-r>", run_code, { noremap = true, silent = true })
+ -- vim.keymap.set("n", "<A-r>", run_code, { noremap = true, silent = true })
 end
 
 return M
--- lua/autorun/config.lua
+++ lua/autorun/config.lua
@@ -2,7 +2,6 @@ local conf = {
   py_exec = "python",
   cpp_c = "clang++",
   c_c = "clang",
-  cpp_dap_c = "clang++",
 }
 
 return conf
